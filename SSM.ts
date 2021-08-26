0cd0ac7f035ed98317998890eb9d799b33624891
patternstormdf <claudi.paniagua@devfactory.com>
2021-08-26 19:33:13 +0200

    First Commit

--- SSM.ts
+++ SSM.ts
@@ -1,15 +1,111 @@
-import ssmcode from "./SSM.json"
 import * as AWS from "aws-sdk";
 import {SDK} from "./SDK";
 import {delay, isDefined} from "./Utils";
 import {Exception} from "./Exception";
+import {AutomationParameterValueList} from "aws-sdk/clients/ssm";
 
 export namespace SSM {
 
-    export async function createDocument(sdk: SDK.Session, owner: string, purpose: string): Promise<Document> {
+    export type Code = {
+        schemaVersion: "0.3"
+        assumeRole?: string
+        description?: string
+        parameters?: Code.Parameters
+        mainSteps: Code.Steps
+        outputs: [string]
+    }
+
+    export namespace Code {
+
+        export namespace Parameter {
+
+            interface BaseParam<T> {
+                type: "String" | "StringList" | "Integer" | "Boolean" | "MapList" | "StringMap"
+                description?: string
+                default?: T
+                allowedValues?: Array<T>
+            }
+
+            export interface String extends BaseParam<string> {
+                type: "String"
+            }
+
+            export interface Integer extends BaseParam<number> {
+                type: "Integer"
+            }
+
+            export interface Boolean extends BaseParam<boolean> {
+                type: "Boolean"
+            }
+
+        }
+
+        type Parameter = Parameter.String | Parameter.Integer | Parameter.Boolean
+
+        export type Parameters = {
+            [key: string]: Parameter
+        }
+
+        export namespace Step {
+
+            interface BaseStep {
+                name: string
+                action: "aws:sleep" | "aws:executeAwsApi"
+                maxAttempts?: number
+                timeoutSeconds?: number
+                onFailure?: "Abort" | "Continue" | string
+                onCancel?: "Abort" | string
+                isEnd?: boolean
+                nextStep?: string
+                isCritical?: boolean
+                inputs: Step.Inputs
+            }
+
+            export type Input = {}
+            export type Inputs = Sleep.Inputs | ExecuteAWSApi.Inputs
+
+            export interface Sleep extends BaseStep {
+                action: "aws:sleep"
+                inputs: Sleep.Inputs
+            }
+
+            export namespace Sleep {
+                export type Inputs = {
+                    Duration: string
+                }
+            }
+
+            export interface ExecuteAWSApi extends BaseStep {
+                action: "aws:executeAwsApi"
+                inputs: ExecuteAWSApi.Inputs
+                outputs: [{
+                    Name: string,
+                    Selector: string
+                    Type: string
+                }]
+            }
+
+            export namespace ExecuteAWSApi {
+                export type Service = "rds" | "ec2"
+
+                export type Inputs = {
+                    Service: Service
+                    Api: string
+                    [key: string]: any
+                }
+
+
+            }
+        }
+
+        export type Step = Step.Sleep | Step.ExecuteAWSApi
+        export type Steps = Array<Step>
+    }
+
+    export async function createRunbook(sdk: SDK.Session, owner: string, purpose: string, code: SSM.Code): Promise<Runbook> {
         const ssm: AWS.SSM = sdk.client(AWS.SSM)
         const params: AWS.SSM.Types.CreateDocumentRequest = {
-            Content: JSON.stringify(ssmcode),
+            Content: JSON.stringify(code),
             Name: `${owner}-${purpose}-SSM-Document`,
             DocumentType: "Automation",
             TargetType: "/",
@@ -20,25 +116,25 @@ export namespace SSM {
         }
         const output: AWS.SSM.Types.CreateDocumentResult = await ssm.createDocument(params).promise()
         if (isDefined(output.DocumentDescription)) {
-            if (isDefined(output.DocumentDescription.Name)) return new Document(ssm, output.DocumentDescription.Name)
-            else throw new CreateDocumentException("no document name returned")
+            if (isDefined(output.DocumentDescription.Name)) return new Runbook(ssm, output.DocumentDescription.Name)
+            else throw new CreateRunbookException("no document name returned")
         }
-        else throw new CreateDocumentException("no document description returned")
+        else throw new CreateRunbookException("no document description returned")
     }
 
-    export class CreateDocumentException extends Exception {
+    export class CreateRunbookException extends Exception {
         constructor(message: string) {
             super(`AWS.SSM.createDocument: ${message}`);
         }
     }
 
-    export namespace Document {
+    export namespace Runbook {
         export type Output = {
             [key: string]: Array<string>
         }
     }
 
-    export class Document {
+    export class Runbook {
         ssm: AWS.SSM
         name: string
         executionId?: string
@@ -48,7 +144,7 @@ export namespace SSM {
             this.name = name
         }
 
-        private async waitForExecutionTermination(executionId: string) : Promise<Document.Output> {
+        private async waitForExecutionTermination(executionId: string) : Promise<Runbook.Output> {
             const output: AWS.SSM.Types.GetAutomationExecutionResult = await this.ssm.getAutomationExecution({AutomationExecutionId: executionId}).promise()
             if (isDefined(output.AutomationExecution)) {
                 if (isDefined(output.AutomationExecution.AutomationExecutionStatus)) {
@@ -65,28 +161,26 @@ export namespace SSM {
                                     ...output.AutomationExecution.Outputs,
                                     ...{status: [output.AutomationExecution.AutomationExecutionStatus]}
                                 }
-                            } else throw new Document.GetExecutionException(this.name, "no outputs returned")
+                            } else throw new Runbook.GetExecutionException(this.name, "no outputs returned")
                         }
                         default: {
                             await delay(1000)
                             return await this.waitForExecutionTermination(executionId)
                         }
                     }
-                } else throw new Document.GetExecutionException(this.name, "no execution status returned")
-            } else throw new Document.GetExecutionException(this.name, "no execution description returned")
+                } else throw new Runbook.GetExecutionException(this.name, "no execution status returned")
+            } else throw new Runbook.GetExecutionException(this.name, "no execution description returned")
         }
 
-        async execute(role: string, clusterId: string): Promise<Document.Output> {
+        //TODO generalize parameters
+        async execute(parameters: {[key: string]: [string]}): Promise<Runbook.Output> {
             const params: AWS.SSM.Types.StartAutomationExecutionRequest = {
                 DocumentName: this.name,
-                Parameters: {
-                    AutomationAssumeRole: [role],
-                    ClusterId: [clusterId]
-                }
+                Parameters: parameters
             }
             const output: AWS.SSM.Types.StartAutomationExecutionResult  = await this.ssm.startAutomationExecution(params).promise()
             if (isDefined(output.AutomationExecutionId)) return await this.waitForExecutionTermination(output.AutomationExecutionId)
-            else throw new Document.StartExecutionException(this.name, "no execution id returned")
+            else throw new Runbook.StartExecutionException(this.name, "no execution id returned")
         }
 
         async delete(): Promise<void> {
---
+++ test/cenpro-24159.test.ts
@@ -0,0 +1,73 @@
+import {SDK} from "../SDK";
+import {Region} from "../Region";
+import {SSM} from "../SSM";
+
+const account: string = "162174280605"  //P2VPTPMsCentralProduct
+const region: Region = new Region("us-east-1")
+const profile: string = "saas-cost-optimization-2" //Change to your AWS profile with your credentials
+const role = "ESW-CO-PowerUser-P2" //Change if you want to use another role
+const owner = "cpaniagua" //Change to your ADFS user identifier. Used to tag created resources.
+const purpose = "CENPRO-24159"
+
+
+test('test runbook', async (done) => {
+
+    const code: SSM.Code = {
+        schemaVersion: "0.3",
+        assumeRole: "{{ AutomationAssumeRole }}",
+        parameters: {
+            AutomationAssumeRole: {
+                type: "String"
+            },
+            ClusterId: {
+                type: "String"
+            }
+        },
+        mainSteps: [
+            {
+                name: "sleep",
+                action: "aws:sleep",
+                inputs: {
+                    "Duration": "PT2S"
+                },
+                nextStep: "describeCluster"
+            },
+            {
+                name: "describeCluster",
+                action: "aws:executeAwsApi",
+                inputs: {
+                    Service: "rds",
+                    Api: "DescribeDBClusters",
+                    Filters: [{
+                        Name: "db-cluster-id",
+                        Values: ["{{ ClusterId }}"]}
+                    ]
+                },
+                outputs: [
+                    {
+                        Name: "ClusterId",
+                        Selector: "$.DBClusters[0].DBClusterIdentifier",
+                        Type: "String"
+                    }
+                ],
+                isEnd: true
+            }
+        ],
+        outputs: ["describeCluster.ClusterId"]
+    }
+
+    SDK.profile(profile)
+    const sdk: SDK.Session = await SDK.Session.init(account, region, role)
+    const runbook: SSM.Runbook = await SSM.createRunbook(sdk, owner, purpose, code)
+    try {
+        console.log("Executing the SSM Runbook...")
+        const output: SSM.Runbook.Output = await runbook.execute({
+            AutomationAssumeRole: [`arn:aws:iam::${account}:role/${role}`],
+            ClusterId: ["lm-conn-pool-test-pg-serverless"]
+        })
+        console.log(JSON.stringify(output))
+    } finally {
+        await runbook.delete()
+    }
+    done();
+}, 30000)
